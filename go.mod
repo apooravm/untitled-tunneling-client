d72324bc44bffc30f94434598cddb0bd53f49d1e
apooravm <medal.apoorav@gmail.com>
2025-02-13 15:48:14 +0000

    add: AHHHHHHHHHHHHHHHHH

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
-module github.com/apooravm/untitled-tunneling
+module github.com/apooravm/untitled-tunneling-client
 
 go 1.21.3
+
+require github.com/gorilla/websocket v1.5.3
--- src/main.go
+++ src/main.go
@@ -1,7 +1,59 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"io"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+var serverAddr = "ws://localhost:4000/api/tunnel"
 
 func main() {
-	fmt.Println("fkjnsfj")
+	localAddr := flag.String("local", "localhost:8081", "Local service to expose.")
+	flag.Parse()
+
+	conn, _, err := websocket.DefaultDialer.Dial(serverAddr, nil)
+	if err != nil {
+		log.Fatal("Failed to connect to server. ", err.Error())
+		return
+	}
+
+	defer conn.Close()
+
+	log.Println("Connection live")
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		reqData, err := io.ReadAll(r.Body)
+		if err != nil {
+			http.Error(w, "Failed to read req", http.StatusInternalServerError)
+		}
+
+		defer r.Body.Close()
+
+		err = conn.WriteMessage(websocket.BinaryMessage, reqData)
+		if err != nil {
+			http.Error(w, "Failed to send request", http.StatusInternalServerError)
+			return
+		}
+
+		_, respData, err := conn.ReadMessage()
+		if err != nil {
+			http.Error(w, "Failed to read request", http.StatusInternalServerError)
+			return
+		}
+
+		w.Write(respData)
+	})
+
+	log.Println("Client proxy running at:", *localAddr)
+	log.Fatal(http.ListenAndServe(*localAddr, nil))
+}
+
+func fatal(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
